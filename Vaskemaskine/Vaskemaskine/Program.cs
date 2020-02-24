e2dec6ea2a4ea3fa60204b2356bf48db6fb13306
KavTV <KavTV@users.noreply.github.com>
2020-02-24 12:36:03 +0100

    Vaskemaskine OOP

---
+++ Vaskemaskine/Vaskemaskine/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vaskemaskine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Washingmachine Wash = new Washingmachine();
+            while (true)
+            {
+                Console.WriteLine(Environment.NewLine + "Tænd/Sluk(1) Fill(2) Wash(3) ECO Wash(4) Spin(5)");
+                int input = Int32.Parse(Console.ReadLine());
+                switch (input)
+                {
+                    case 1:
+                        Wash.TurnItOn();
+                        break;
+                    case 2:
+                        Wash.Fill();
+                        break;
+                    case 3:Wash.WashMethod();
+                        break;
+                    case 4:Wash.EcoWash();
+                        break;
+                    case 5:Wash.Spin();
+                        break;
+                    
+                    default: return;
+
+                }
+            }
+
+            
+
+        }
+    }
+}
---
+++ Vaskemaskine/Vaskemaskine/Washingmachine.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Vaskemaskine
+{
+    public class Washingmachine
+    {
+        bool turnedon;
+        
+
+        public void TurnItOn()
+        {
+            if (turnedon == true)
+            {
+                turnedon = false;
+                Console.WriteLine("Vaskemaskine slukkes");
+            }
+            else
+            {
+                turnedon = true;
+                Console.WriteLine("Vaskemaskine tændt");
+            }
+            
+        }
+        
+
+        public void Fill()
+        {
+            if (turnedon == false)
+            {
+                Console.WriteLine("Vaskemaskine ikke tændt");
+                return;
+            }
+            for (int i = 10; i >= 1; i--)
+            {
+                Thread.Sleep(500);
+                Console.WriteLine("Fylder vand i...{0}",i);
+            }
+        }
+
+        public void WashMethod()
+        {
+            if (turnedon == false)
+            {
+                Console.WriteLine("Vaskemaskine ikke tændt");
+                return;
+            }
+            for (int i = 15; i >= 1; i--)
+            {
+                Thread.Sleep(500);
+                Console.WriteLine("Vasker...{0}", i);
+            }
+        }
+
+        public void EcoWash()
+        {
+            if (turnedon == false)
+            {
+                Console.WriteLine("Vaskemaskine ikke tændt");
+                return;
+            }
+            for (int i = 20; i >= 1; i--)
+            {
+                Thread.Sleep(500);
+                Console.WriteLine("Eco Vasker...{0}", i);
+            }
+        }
+
+        public void Spin()
+        {
+            if (turnedon == false)
+            {
+                Console.WriteLine("Vaskemaskine ikke tændt");
+                return;
+            }
+            for (int i = 15; i >= 1; i--)
+            {
+                Thread.Sleep(500);
+                Console.WriteLine("Drejer rundt...{0}", i);
+            }
+        }
+
+    }
+}
