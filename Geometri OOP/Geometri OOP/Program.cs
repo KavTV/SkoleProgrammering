cf0e7ab5b70a07f386b6af0b05bfd02832060b9b
KavTV <KavTV@users.noreply.github.com>
2020-02-24 10:52:52 +0100

    Geometri OOP

---
+++ Geometri OOP/Geometri OOP/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometri_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv siden");
+            double siden = double.Parse( Console.ReadLine());
+            Square S = new Square(siden);
+            Square D = new Square();
+
+            double perimeter = S.Perimeter();
+            double area = S.Area();
+            Console.WriteLine(perimeter);
+            Console.WriteLine(area);
+            Console.WriteLine("--------------");
+            Console.WriteLine(D.Perimeter());
+            Console.WriteLine(D.Area());
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Geometri OOP/Geometri OOP/Square.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometri_OOP
+{
+    public class Square
+    {
+        double side = 0;
+
+        // Lav en konstruktør
+        public Square(double side)
+        {
+            this.side = side;
+        }
+        public Square()
+        {
+            
+        }
+        //property
+        public double Side
+        {
+            get { return side; }
+            set { side = value; }
+
+        }
+        //beregn omkreds og return
+        public double Perimeter()
+        {
+            double result = this.side * 4;
+            return result;
+        }
+        //beregn areal og return
+        public double Area()
+        {
+            double result = this.side * this.side;
+            return result;
+        }
+    }
+}
+
