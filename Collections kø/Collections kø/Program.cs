d1adc7715ba206424baae991e59d5c6a7f7b6bbe
KavTV <KavTV@users.noreply.github.com>
2020-02-25 10:31:09 +0100

    ting

---
+++ Collections kø/Collections kø/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections_kø
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Queue<int> primes = new Queue<int>();
+
+            primes.Enqueue(2);
+            primes.Enqueue(3);
+            primes.Enqueue(5);
+            primes.Enqueue(7);
+            primes.Enqueue(11);
+
+            int total = 0;
+
+            for (int i = 0; i < primes.Count; i++)
+            {
+                total++;
+            }
+            Console.WriteLine(total);
+
+            Console.ReadLine();
+        }
+    }
+}
