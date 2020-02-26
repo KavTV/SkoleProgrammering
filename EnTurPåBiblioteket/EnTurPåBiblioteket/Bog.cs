3ac3914f23fcb556966774a1fd34e56dff6c3985
KavTV <KavTV@users.noreply.github.com>
2020-02-26 13:27:02 +0100

    Bibliotek

---
+++ EnTurPåBiblioteket/EnTurPåBiblioteket/Bog.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnTurPåBiblioteket
+{
+    class Bog
+    {
+
+        int sides;
+        bool hardCover;
+        string category;
+        string name;
+        static List<Bog> Books = new List<Bog>();
+        
+        public int Sides
+        {
+            get { return sides; }
+            set { sides = value; }
+        }
+        public bool HardCover { get => hardCover; set => hardCover = value; }
+        public string Category { get => category; set => category = value; }
+        public string Name { get => name; set => name = value; }
+
+        public Bog()
+        {
+
+        }
+        public Bog(string theName, int side, bool isHardCover, string Cat )
+        {
+            this.sides = side;
+            this.hardCover = isHardCover;
+            this.category = Cat;
+            this.name = theName;
+        }
+
+        public void addBooks()
+        {
+            Books.Add(new Bog("Flyvende Jens",200,true,"Fantasy"));
+            Books.Add(new Bog("Dragen der ikke kunne flyve", 50, false, "Fantasy" ));
+            Books.Add(new Bog("Mordet",500, true, "Krimi" ));
+            Books.Add(new Bog("Cisco network",10000, true, "Kedeligt" ));
+            Books.Add(new Bog("Harry potter", 200, true, "Fantasy"));
+            
+        }
+
+        public void ShowBooks()
+        {
+            foreach (var item in Books)
+            {
+                string hardCoverString = "Hårdt omslag";
+                if (item.hardCover == true)
+                {
+                    hardCoverString = "Blødt omslag";
+                }
+                Console.WriteLine("{0}, er i kategorien {1} og har {2} sider og {3}", item.name, item.category, item.sides, hardCoverString);
+            }
+        }
+
+        public Bog LendBook(string bookName)
+        {
+            
+            foreach (var item in Books)
+            {
+                if (item.name == bookName)
+                {
+                    Books.Remove(item);
+                    return item;
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ EnTurPåBiblioteket/EnTurPåBiblioteket/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnTurPåBiblioteket
+{
+    class Program
+    {
+           static Bog Book = new Bog();
+        static void Main(string[] args)
+        {
+            
+                Book.addBooks();
+            while (true)
+            {
+                Console.WriteLine("Vælg bog du vil låne(1) Se bøger der kan lånes(2) vis dine lånte bøger(3) Lån valgte bøger(4)");
+                int input1 = Int32.Parse(Console.ReadLine());
+                switch (input1)
+                {
+                    case 1: LendSystem();
+                        break;
+                    case 2: Book.ShowBooks();
+                        break;
+                    case 3: showLend();
+                        break;
+                    case 4: 
+                        break;
+                    default:
+                        break;
+                }
+                
+                Console.WriteLine(Environment.NewLine);
+
+            }
+
+
+        }
+
+
+        static Stack<Bog> BookStack = new Stack<Bog>();
+        private static void LendSystem()
+        {
+            Console.WriteLine("Skriv navnet på bog");
+            string bookName = Console.ReadLine();
+            Bog lendTrue = Book.LendBook(bookName);
+            if (lendTrue != null)
+            {
+                BookStack.Push(lendTrue);
+                Console.WriteLine("Bog lånt ud!");
+            }
+            else
+            {
+                Console.WriteLine("Kunne ikke finde nogle bog");
+            }
+        }
+
+        static void showLend()
+        {
+            Console.WriteLine(Environment.NewLine);
+            foreach (var item in BookStack)
+            {
+                Console.WriteLine(item.Name);
+            }
+        }
+        
+
+    }
+}
