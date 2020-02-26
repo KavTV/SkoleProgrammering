dd38a61d6c549c47eb2eba1b7cf729eb0e336d13
KavTV <46051618+KavTV@users.noreply.github.com>
2020-02-26 15:52:34 +0100

    færdig

--- DictionaryJediMindTricks/DictionaryJediMindTricks/Program.cs
+++ DictionaryJediMindTricks/DictionaryJediMindTricks/Program.cs
@@ -10,7 +10,21 @@ namespace DictionaryJediMindTricks
     {
         static void Main(string[] args)
         {
-            Dictionary<string, int> people = new Dictionary<string, int>();
+            //Opret Dictionary
+            Dictionary<string, int> people = new Dictionary<string, int>() { {"Jens",20 }, {"Han",70 } };
+            //Tilføj til Dictionary
+            people.Add("Kasper", 17);
+            //First() finder den første ting inde i Dictionary people
+            Console.WriteLine(people.First());
+
+            people.Remove("Han");
+            Console.WriteLine("--------------");
+            foreach (var item in people)
+            {
+                Console.WriteLine("{0} er {1} år",item.Key, item.Value);
+            }
+            Console.WriteLine("--------------");
+            Console.ReadLine();
         }
     }
 }
